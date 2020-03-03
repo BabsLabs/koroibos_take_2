4b7c27ab0416dbee3accf6ba685e06c48cf4b637
Brian Bower <brian@brianbower.com>
2020-03-03 12:06:33 -0700

    Add many to many with OlympainSports - this might get redone

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -2,6 +2,8 @@ class Olympian < ApplicationRecord
   has_many :medals
   has_many :olympian_events
   has_many :events, through: :olympian_events
+  has_many :olympian_sports
+  has_many :sports, through: :olympian_sports
 
   validates_presence_of :name,
                         :sex,
