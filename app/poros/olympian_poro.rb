8a67d0fdca992fc1c4ce60b93302a5aaeb61d1e8
Brian Bower <brian@brianbower.com>
2020-03-03 12:42:07 -0700

    Refactor sports relationship and fix typo

--- app/poros/olympian_poro.rb
+++ app/poros/olympian_poro.rb
@@ -4,8 +4,8 @@ class OlympianPORO
     @name = olympian_info.name
     @team = olympian_info.team
     @age = olympian_info.age
-    @sport = olympian_info.sport
-    @total_meadls_won = olympian_info.medals.count
+    @sport = olympian_info.sport.sport
+    @total_medals_won = olympian_info.medals.count
   end
 
 end
